6aded268462b02f8f493300ed33dc5aed1806ac5
jd_786 <jaydeep_shil@certree.com>
2024-03-02 13:44:06 +0530

    simple solutuin

--- Combination Sum/Program.cs
+++ Combination Sum/Program.cs
@@ -3,43 +3,50 @@ using System.Collections.Generic;
 
 namespace Combination_Sum
 {
-  //
-  class Program
-  {
-    static void Main(string[] args)
+    //https://www.youtube.com/watch?v=GBKI9VSKdGg
+    class Program
     {
-      var nums = new int[] { 2, 3, 5 };
-      int target = 8;
-      Program p = new Program();
-      var result = p.CombinationSum(nums, target);
-      foreach (var res in result)
-        Console.WriteLine(string.Join(",", res));
-    }
+        static void Main(string[] args)
+        {
+            var nums = new int[] { 2, 3, 6, 7 };
+            int target = 7;
+            Solution p = new Solution();
+            var result = p.CombinationSum(nums, target);
+            foreach (var res in result)
+                Console.WriteLine(string.Join(",", res));
+        }
+        public class Solution
+        {
+            public IList<IList<int>> CombinationSum(int[] candidates, int target)
+            {
+                var res = new List<IList<int>>();
+                var temp = new List<int>();
+                void Dfs(int i, int total)
+                {
+                    // base case
+                    if (total == target)
+                    {
+                        res.Add(new List<int>(temp));
+                        return;
+                    }
+                    // if index out of ound
+                    if (i >= candidates.Length || total > target)
+                    {
+                        return;
+                    }
 
-    public IList<IList<int>> CombinationSum(int[] nums, int target)
-    {
-      var result = new List<IList<int>>();
-      //Array.Sort(nums);
-      Backtracking(result, nums, target, new List<int>(), 0, 0);
-      return result;
+                    temp.Add(candidates[i]);
+                    total += candidates[i];
+                    // add the same no multiple times
+                    Dfs(i, total);
+                    var val = temp[temp.Count - 1];
+                    temp.RemoveAt(temp.Count - 1);
+                    // add next no
+                    Dfs(i + 1, total - val);
+                }
+                Dfs(0, 0);
+                return res;
+            }
+        }
     }
-
-    private void Backtracking(List<IList<int>> result, int[] nums, int target, IList<int> temp, int start, int currentSum)
-    {
-      if (currentSum > target) return;
-      if (currentSum == target)
-      {
-        result.Add(new List<int>(temp));
-        return;
-      }
-      for (int i = start; i < nums.Length; i++)
-      {
-        temp.Add(nums[i]);
-        currentSum += nums[i];
-        Backtracking(result, nums, target, temp, i, currentSum);
-        temp.RemoveAt(temp.Count - 1);
-        currentSum -= nums[i];
-      }
-    }
-  }
 }
